3dd5c9130a5eb62da6b358df4db210c2c1f970dd
metacode22 <alohajune22@gmail.com>
2022-09-13 21:10:24 +0900

    chore: 프로젝트 기본 환결 설정

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="App">github issues</div>;
+}
+
+export default App;
---
+++ src/styles/themes/colors.js
@@ -0,0 +1,8 @@
+const colors = {
+  white: '#ffffff',
+  black: '#000000',
+  gray1: '#212121',
+  gray2: '#484848',
+};
+
+export default colors;
